3f3acaef8cec55b357a3448e48d44ad1dfb16db5
Anatoly Korbasov <anatolykorbasov@gmail.com>
2023-02-02 23:27:43 +0300

    Test passed

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,12 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+
+    @Override
+    public String toString() {
+        return "\r\n Вот и сказочке конец. \r\n А кто слушал - молодец";
+    }
 }
